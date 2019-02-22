3ac256b6dc3100ceec7771b71ea3cd96f3e4be12
szmotz <szmotz@gmail.com>
2019-02-22 01:28:03 +0100

    zobaczynmy jak się zachowujecie

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,6 @@ Route::get('/', function () {
 Route::get('/contact', 'PagesController@contact');
 Route::get('/about', 'PagesController@about');
 
+Route::get('/products', 'ProductsController@index');
+
+
